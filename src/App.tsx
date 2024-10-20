104854f344a2a77152bfde0ae91707c0fb385828
gerardocrr <gerardo2551.cr@gmail.com>
2024-10-19 23:38:22 -0600

    unused code and comments in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,32 @@
-import { UploadWidget } from "./components/UploadWidget";
-import { AdvancedImage } from "@cloudinary/react";
-import {
-  Cloudinary,
-  CloudinaryImage,
-  Transformation,
-} from "@cloudinary/url-gen/index";
-import {
-  generativeReplace,
-  backgroundRemoval,
-} from "@cloudinary/url-gen/actions/effect";
-import { useState } from "react";
-import { source } from "@cloudinary/url-gen/actions/overlay";
-import { image } from "@cloudinary/url-gen/qualifiers/source";
-import { scale } from "@cloudinary/url-gen/actions/resize";
-import { Position } from "@cloudinary/url-gen/qualifiers";
-import { focusOn } from "@cloudinary/url-gen/qualifiers/gravity";
-import { advancedFaces } from "@cloudinary/url-gen/qualifiers/focusOn";
+// import { UploadWidget } from "./components/UploadWidget";
+// import { AdvancedImage } from "@cloudinary/react";
+// import {
+//   Cloudinary,
+//   CloudinaryImage,
+//   Transformation,
+// } from "@cloudinary/url-gen/index";
+// import {
+//   generativeReplace,
+//   backgroundRemoval,
+// } from "@cloudinary/url-gen/actions/effect";
+// import { useState } from "react";
+// import { source } from "@cloudinary/url-gen/actions/overlay";
+// import { image } from "@cloudinary/url-gen/qualifiers/source";
+// import { scale } from "@cloudinary/url-gen/actions/resize";
+// import { Position } from "@cloudinary/url-gen/qualifiers";
+// import { focusOn } from "@cloudinary/url-gen/qualifiers/gravity";
+// import { advancedFaces } from "@cloudinary/url-gen/qualifiers/focusOn";
 import { Background } from "./components/Background";
-import "./App.css";
+// import "./App.css";
 
 function App() {
-  const [publicID, setPublicID] = useState("");
-  const cld = new Cloudinary({
-    cloud: {
-      cloudName: import.meta.env.VITE_PUBLIC_CLOUDINARY_CLOUD_NAME,
-    },
-  });
-  const myImage = cld.image(publicID);
+  // const [publicID, setPublicID] = useState("");
+  // const cld = new Cloudinary({
+  //   cloud: {
+  //     cloudName: import.meta.env.VITE_PUBLIC_CLOUDINARY_CLOUD_NAME,
+  //   },
+  // });
+  // const myImage = cld.image(publicID);
 
   // const imageMask = cld
   //   .image(publicID)
@@ -34,15 +34,15 @@ function App() {
   //     generativeReplace().from("face").to("the walking dead zombie mask")
   //   );
 
-  const imageMask = cld
-    .image(publicID)
-    .overlay(
-      source(
-        image("hackathon/masks/yigdnszx6gwet1vevzzo").transformation(
-          new Transformation().resize(scale().width(1.9).regionRelative())
-        )
-      ).position(new Position().gravity(focusOn(advancedFaces())))
-    );
+  // const imageMask = cld
+  //   .image(publicID)
+  //   .overlay(
+  //     source(
+  //       image("hackathon/masks/yigdnszx6gwet1vevzzo").transformation(
+  //         new Transformation().resize(scale().width(1.9).regionRelative())
+  //       )
+  //     ).position(new Position().gravity(focusOn(advancedFaces())))
+  //   );
 
   return (
     <>
