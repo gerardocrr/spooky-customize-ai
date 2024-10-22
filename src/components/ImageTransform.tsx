81018a3e1afbb06d1fae35233263ea569c9062c7
gerardocrr <gerardo2551.cr@gmail.com>
2024-10-21 20:11:20 -0600

    Refactor: Update route path for image transformation in ImageTransform component

--- src/components/ImageTransform.tsx
+++ src/components/ImageTransform.tsx
@@ -1,21 +1,25 @@
 import { useState } from "react";
-// import { UploadWidget } from "./UploadWidget";
-import { Cloudinary } from "@cloudinary/url-gen/index";
+import { Cloudinary, Transformation } from "@cloudinary/url-gen/index";
 import { AdvancedImage } from "@cloudinary/react";
-// import { source } from "@cloudinary/url-gen/actions/overlay";
-// import { image } from "@cloudinary/url-gen/qualifiers/source";
-// import { scale } from "@cloudinary/url-gen/actions/resize";
-// import { Position } from "@cloudinary/url-gen/qualifiers/position";
-// import { focusOn } from "@cloudinary/url-gen/qualifiers/gravity";
-// import { faces } from "@cloudinary/url-gen/qualifiers/focusOn";
+import { useParams } from "react-router-dom";
+import { source } from "@cloudinary/url-gen/actions/overlay";
+import { image } from "@cloudinary/url-gen/qualifiers/source";
+import { scale } from "@cloudinary/url-gen/actions/resize";
+import { Position } from "@cloudinary/url-gen/qualifiers/position";
+import { focusOn } from "@cloudinary/url-gen/qualifiers/gravity";
+import { faces } from "@cloudinary/url-gen/qualifiers/focusOn";
+import { CloudinaryImage } from "@cloudinary/url-gen/index";
 
 interface Props {
   currentIndex: number;
 }
 
 export function ImageTransform({ currentIndex }: Props) {
-  //const [publicID, setPublicID] = useState("");
   const [isHoverButton, setIsHoverButton] = useState(false);
+  const [imageTransformed, setImageTransformed] = useState<
+    CloudinaryImage | undefined
+  >(undefined);
+  const params = useParams();
 
   const emojis = ["🎈", "👻", "✌️", "🏒", "😊"];
 
@@ -25,28 +29,39 @@ export function ImageTransform({ currentIndex }: Props) {
     },
   });
 
-  const myImage = cld.image("bwa9xdsy3pib90jom5yb");
+  const myImage = cld.image(params.id);
 
-  // const imageMask = cld
-  //   .image(publicID)
-  //   .overlay(
-  //     source(
-  //       image("hackathon/masks:fdmus1lybdzzm95mkx0e").transformation(
-  //         new Transformation().resize(scale().width(1.5).regionRelative())
-  //       )
-  //     ).position(new Position().gravity(focusOn(faces())))
-  //   );
+  const handleTransform = () => {
+    const image2 = cld
+      .image(params.id)
+      .overlay(
+        source(
+          image("hackathon/masks:fdmus1lybdzzm95mkx0e").transformation(
+            new Transformation().resize(scale().width(1.5).regionRelative())
+          )
+        ).position(new Position().gravity(focusOn(faces())))
+      );
+    setImageTransformed(image2);
+  };
 
   return (
     <div className="flex flex-col justify-center items-center">
-      {/* <UploadWidget setImage={setPublicID} /> */}
-
       <div className="flex flex-col justify-center items-center">
-        <AdvancedImage
-          className="rounded-lg mb-5"
-          cldImg={myImage}
-          width={"40%"}
-        />
+        {!imageTransformed && (
+          <AdvancedImage
+            className="rounded-lg mb-5"
+            cldImg={myImage}
+            width={"40%"}
+          />
+        )}
+
+        {imageTransformed && (
+          <AdvancedImage
+            className="rounded-lg mb-5"
+            cldImg={imageTransformed}
+            width={"40%"}
+          />
+        )}
         <button
           className={`bg-orange-600 p-2 rounded-lg text-white transition-all duration-300 ease-in-out ${
             isHoverButton ? "scale-110" : "scale-100"
@@ -56,12 +71,11 @@ export function ImageTransform({ currentIndex }: Props) {
           }}
           onMouseEnter={() => setIsHoverButton(true)}
           onMouseLeave={() => setIsHoverButton(false)}
+          onClick={handleTransform}
         >
           {emojis[currentIndex]} Transform!
         </button>
       </div>
-
-      {/* <AdvancedImage cldImg={imageMask} width={"40%"} /> */}
     </div>
   );
 }
