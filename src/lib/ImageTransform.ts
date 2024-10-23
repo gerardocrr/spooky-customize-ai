e76cc10f5a7387c8c00eaef7bff8237a620fd2c0
gerardocrr <gerardo2551.cr@gmail.com>
2024-10-22 19:59:58 -0600

    Refactor: Update background images in ImageTransform component

--- src/lib/ImageTransform.ts
+++ src/lib/ImageTransform.ts
@@ -29,39 +29,39 @@ const masks = [
   "s4wlthv4ay69fbuljlmd",
 ];
 const itBackgrounds = [
-  "vulatg3z5a53xycmkhxv",
-  "bltxtaedxsjebidugrza",
-  "tjguqc2sbxjlk2epetgl",
-  "yltahvidkvq7iuavbvff",
-  "jydeak0pslokqmmhswxd",
+  "neiqm9fxrcolgjy3opap",
+  "ciipkspjyiuvhbtkcnzg",
+  "n2jfdklvk00n5tsusxrl",
+  "bsucfmfi7khgbrkikqch",
+  "yngbyk0lpjpvs46dxsua",
 ];
 const screamBackgrounds = [
-  "atlbfntsxh5pxhw7f7x1",
-  "drif5nhrs08we56orpzn",
-  "mszjsvd4re9cgmn6hct0",
-  "iei5p7kshh3zzzu0paxj",
-  "jsfjyqpooajhimyclrov",
+  "ialnmqnyqzru2pjxy3qr",
+  "lfkt6huzuykw3ahiy9wd",
+  "kc5o15bcrzvblb6db4ut",
+  "y2n8rqbtmy4qoaj1gdds",
+  "itrspkhdkirphs3nazo4",
 ];
 const terrifierBackgrounds = [
-  "axk1dgwgbgmseq6oqkom",
-  "innqqsqn9tpbfe6ht96z",
-  "mujxd4jwghm7yw0bfjgc",
-  "xqs3ul6bpwbm09qp2zny",
-  "ozfupzgwwp8xgc3hwuq8",
+  "anwchofgzyhe3vwhtglz",
+  "g63dq0nwweyo3ycrflbz",
+  "srm6gr9kjviocgxy0sd8",
+  "cmx16pvgwxjkhjgp81lw",
+  "l4gbyc7jzxdfdqrdnutf",
 ];
 const sawBackgrounds = [
-  "azo4gg4u9i6x5pnew5kw",
-  "pekb2o8uqrucntdjgelt",
-  "e2ejjgnebfonakignyf8",
-  "mdqjgpgpyawqptck6ila",
-  "wgvdmqev78pesslacbpq",
+  "ixo7ac10srlstmjwbdef",
+  "iwi7j3zk3evjan61m1fa",
+  "nupp4bquxx4hmtmmnokp",
+  "ykxzskj2to3xfx9rmex0",
+  "ymbxjjnoa3mh87eptdch",
 ];
 const friday13thBackgrounds = [
-  "ihfplcjnyjihdpugkeve",
-  "htk1s8amrklxsmtstpcd",
-  "sritjicwju3fa31cpqby",
-  "b6fsyyncm28ivpn28tya",
-  "fglp9arzpbforkiynygm",
+  "dhebteb5s8tc2jsdmwot",
+  "ygqhyueddtz8mytbin9x",
+  "a3gmi3wsmq7hoom44j46",
+  "kfbte3bzut4vedqfy2gw",
+  "yekto3yhflzwtpiqnwtz",
 ];
 
 const randomBackground = (index: number) => {
