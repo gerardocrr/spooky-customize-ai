9f7c9c444fa66dde70f6037781dc1e21872adf70
gerardocrr <gerardo2551.cr@gmail.com>
2024-10-22 22:01:15 -0600

    Refactor: Add loader animation to Gallery component

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -13,6 +13,7 @@ interface ImageData {
 
 export function Gallery() {
   const [images, setImages] = useState<ImageData | null>(null);
+  const [isLoading, setIsLoading] = useState(true);
   const navigate = useNavigate();
   useEffect(() => {
     const getData = async () => {
@@ -23,6 +24,7 @@ export function Gallery() {
       );
       const data = await response.json();
       setImages(data);
+      setIsLoading(false);
     };
     getData();
   }, []);
@@ -34,21 +36,27 @@ export function Gallery() {
   });
 
   return (
-    <div className="grid grid-cols-4">
-      {images?.resources?.map((img, idx) => {
-        const myImage = cld.image(img.public_id);
-        return (
-          <div className="p-5" key={idx}>
-            <AdvancedImage
-              className="rounded-lg hover:cursor-pointer"
-              cldImg={myImage}
-              onClick={() => {
-                navigate(`/image/${img.public_id}`);
-              }}
-            />
-          </div>
-        );
-      })}
+    <div>
+      {isLoading ? (
+        <div className="loader text-8xl flex justify-center items-center"></div>
+      ) : (
+        <div className="grid grid-cols-4">
+          {images?.resources?.map((img, idx) => {
+            const myImage = cld.image(img.public_id);
+            return (
+              <div className="p-5" key={idx}>
+                <AdvancedImage
+                  className="rounded-lg hover:cursor-pointer"
+                  cldImg={myImage}
+                  onClick={() => {
+                    navigate(`/image/${img.public_id}`);
+                  }}
+                />
+              </div>
+            );
+          })}
+        </div>
+      )}
     </div>
   );
 }
