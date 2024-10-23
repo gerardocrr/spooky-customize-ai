e445a2bb40d9a9e4dd21ef3071772e6b1b10cffb
gerardocrr <gerardo2551.cr@gmail.com>
2024-10-23 02:01:48 -0600

    Refactor: Improve styling and add instructions in UploadWidget component

--- src/components/UploadWidget.tsx
+++ src/components/UploadWidget.tsx
@@ -26,18 +26,24 @@ export function UploadWidget() {
     );
   }, []);
   return (
-    <button
-      className={`bg-orange-600 p-2 rounded-lg text-white transition-all duration-300 ease-in-out ${
-        isHoverButton ? "scale-110" : "scale-100"
-      }`}
-      style={{
-        boxShadow: isHoverButton ? "0 0 50px rgba(255, 130, 0, 1)" : "none",
-      }}
-      onMouseEnter={() => setIsHoverButton(true)}
-      onMouseLeave={() => setIsHoverButton(false)}
-      onClick={() => (WidgetRef.current as any).open()}
-    >
-      🎃 Upload your image!
-    </button>
+    <div className="flex flex-col justify-center items-center">
+      <p className="text-white mb-5 bg-red-600 p-2 rounded-lg bg-opacity-20">
+        For a better experience use a horizontal image where your face is
+        clearly visible and the background is clear.
+      </p>
+      <button
+        className={`bg-orange-600 p-2 rounded-lg text-white transition-all duration-300 ease-in-out ${
+          isHoverButton ? "scale-110" : "scale-100"
+        }`}
+        style={{
+          boxShadow: isHoverButton ? "0 0 50px rgba(255, 130, 0, 1)" : "none",
+        }}
+        onMouseEnter={() => setIsHoverButton(true)}
+        onMouseLeave={() => setIsHoverButton(false)}
+        onClick={() => (WidgetRef.current as any).open()}
+      >
+        🎃 Upload your image!
+      </button>
+    </div>
   );
 }
